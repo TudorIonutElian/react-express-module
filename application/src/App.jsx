69b3ee63d5703b5d034b4b35fc5faf2cf9986aaf
Tudor Ionut Elian <ionuteliantudor@gmail.com>
2022-11-17 04:28:03 +0200

    Set left right menu side

--- application/src/App.jsx
+++ application/src/App.jsx
@@ -4,13 +4,48 @@ import SidedMenu from "./components/SidedMenu";
 import urls from './data/url';
 
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
-  const [url, setUrl] = useState("");
+  const [url, setUrl] = useState("/");
+  const [apiSearch, setApiSearch] = useState("");
+  const [menuSide, setMenuSide] = useState("left");
+  const [apiData, setApiData] = useState([]);
+  
+  useEffect(() => {
+    // Update the document title using the browser API
+    switch (url) {
+      case urls[0].href:
+        setApiSearch("api1");
+        setMenuSide("left");
+        break;
+  
+      case urls[1].href:
+        setApiSearch("api2");
+        setMenuSide("left");
+        break;
+  
+      case urls[2].href:
+        setApiSearch("api1");
+        setMenuSide("right");
+        break;
+  
+      case urls[3].href:
+        setApiSearch("api2");
+        setMenuSide("right");
+        break;
+    
+      default:
+        break;
+    }
+  }, [url]);
+
+  
 
   const setURLOnClick = (e, newUrl) => {
     e.preventDefault();
-    console.log("URL will change into " + newUrl);
+    setUrl(newUrl);
+    console.log(menuSide)
   }
 
   console.log(url)
@@ -18,7 +53,7 @@ function App() {
     <div className="App h-screen">
       <Header urls={urls} urlChanger={setURLOnClick}/>
       <Items />
-      <SidedMenu urls={urls}/>
+      <SidedMenu urls={urls} menuSide={menuSide} urlChanger={setURLOnClick}/>
     </div>
   )
 }
--- application/src/components/SidedMenu.jsx
+++ application/src/components/SidedMenu.jsx
@@ -1,14 +1,14 @@
-function SidedMenu({urls}) {
+function SidedMenu({urls, menuSide, urlChanger}) {
     const generatedUrls = urls.map((url, index) => {
         return (
             <li className="p-2 w-full text-center hover:bg-blue-900" key={index}>
-                <a href={url.href}>{url.text}</a>
+                <a href={url.href} onClick={()=> {urlChanger(event, url.href)}}>{url.text}</a>
             </li>
         );
     });
 
     return (
-        <div className="h-screen bg-cyan-700 text-white fixed top-0 bottom-0 w-60">
+        <div className={`h-screen bg-cyan-700 text-white fixed top-0 bottom-0 w-60 ${menuSide}-0`}>
            <ul className="flex flex-col justify-center justify-items-center content-between items-center">
                 {generatedUrls}
             </ul>
